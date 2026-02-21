9214a06759f709b880663342f18957313600a37d
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-21 23:17:03 +0530

    this file contain butterfly pattern program

---
+++ self_learning/pattern_program_20feb/butterfly pattern.py
@@ -0,0 +1,27 @@
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n+1):
+    print("*"*i + " "*(2*(n-i)) + "*"*i)
+for i in range(n, 0, -1):
+    print("*"*i + " "*(2*(n-i)) + "*"*i)
+
+
+
+
+
+
+
+
+
+'''Output:
+Enter number of rows: 5
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+    '''
