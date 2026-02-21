ed04af4605715d207f973adb89420091f102b574
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-21 23:37:13 +0530

    this file contain square pattern program

---
+++ self_learning/pattern_program_20feb/square pattern.py
@@ -0,0 +1,17 @@
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+
+
+    
+
+    '''Output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+    '''
