e9aebb3a75e47cedff93052275b174e3e69fa079
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-21 23:33:06 +0530

    this file contain rhombus pattern program

---
+++ self_learning/pattern_program_20feb/rhombus pattern.py
@@ -0,0 +1,14 @@
+n=int(input("enter number of rows:"))
+for i in range(n):
+    print(" " * (n-1) + "*" * n)
+
+
+
+'''output:
+enter number of rows:5
+    *****
+    *****
+    *****
+    *****
+    *****
+'''
