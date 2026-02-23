3d4973ae734c54b9ea6fc78fb17d0df5afebd842
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 01:35:45 +0530

    ths file contain intersection of two sets program

---
+++ self_learning/Python Data Structures__23feb/set_intersection.py
@@ -0,0 +1,17 @@
+# Program to find intersection of two sets
+
+set1 = {1, 2, 3}
+set2 = {2, 3, 4}
+
+# Intersection gives common elements
+result = set1.intersection(set2)
+
+print("Intersection of sets:", result)
+
+
+
+
+#-------------------------------------------------
+'''#output
+Intersection of sets: {2, 3}
+'''
