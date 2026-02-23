9a178cf59e438a63e0f37470eb33b55c65c86f26
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 01:39:16 +0530

    this file contain remove duplicates from list using set program

---
+++ self_learning/Python Data Structures__23feb/remove_duplicates_set.py
@@ -0,0 +1,20 @@
+# Program to remove duplicate elements from a list
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+# Convert list to set (set removes duplicates automatically)
+unique_numbers = set(numbers)
+
+print("List after removing duplicates:", unique_numbers)
+
+
+
+
+
+
+
+
+#-------------------------------------------------
+'''#output
+List after removing duplicates: {1, 2, 3, 4, 5}
+'''
