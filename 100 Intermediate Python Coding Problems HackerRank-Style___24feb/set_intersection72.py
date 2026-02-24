9d9a5a3c7b0ad93cb0936f435dd3c926a79d42f7
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:17:15 +0530

    this file contain set intersection program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/set_intersection72.py
@@ -0,0 +1,16 @@
+#input two sets and find their intersection
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+intersection_set = set1.intersection(set2)
+#display the sets and their intersection
+print(f"Set 1: {set1}")
+print(f"Set 2: {set2}")
+#display the intersection of the two sets
+print(f"Intersection: {intersection_set}")
+
+'''
+Output:
+Set 1: {1, 2, 3, 4, 5}
+Set 2: {4, 5, 6, 7, 8}
+Intersection: {4, 5}
+'''
