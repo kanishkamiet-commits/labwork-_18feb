ed713f2599cbfada71a2e03d8f0377f1eddca023
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:37:43 +0530

    this file contain sum dict values program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/sum_dict_values84.py
@@ -0,0 +1,15 @@
+# Sum all the values in a dictionary
+student_scores = {
+    "Alice": 95,
+    "Bob": 87,
+    "Charlie": 92,
+    "Diana": 98,
+    "Eve": 89
+}
+total = sum(student_scores.values())
+print(f"Sum of all values: {total}")
+
+'''
+Output:
+Sum of all values: 461
+'''
