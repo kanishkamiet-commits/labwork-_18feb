18d7c73f58080f2c8702a2bc218fa6b758e4fa8a
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:18:08 +0530

    this file contain set difference program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/set_difference73.py
@@ -0,0 +1,16 @@
+#input two sets and find their difference
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+difference_set = set1.difference(set2)
+#display the sets and their difference
+print(f"Set 1: {set1}")
+print(f"Set 2: {set2}")
+#display the difference of the two sets
+print(f"Difference: {difference_set}")
+
+'''
+Output:
+Set 1: {1, 2, 3, 4, 5}
+Set 2: {4, 5, 6, 7, 8}
+Difference: {1, 2, 3}
+'''
