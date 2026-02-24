225742c3a67266f93d2fd4b69bf25a73ff4d7ca2
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:12:38 +0530

    this file contain tuple to list program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/tuple_to_list68.py
@@ -0,0 +1,10 @@
+#input a tuple of numbers and convert it to a list
+numbers = (1, 2, 3, 4, 5)
+numbers_list = list(numbers)
+#display the tuple and the list
+print(f"Tuple: {numbers}")
+print(f"List: {numbers_list}")
+
+'''Output:
+Tuple: (1, 2, 3, 4, 5)
+List: [1, 2, 3, 4, 5]'''
