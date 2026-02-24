06eb24d4675093174142293c798d142b07481378
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:11:42 +0530

    this file contain tuple min value program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/tuple_min_value67.py
@@ -0,0 +1,12 @@
+#input a tuple of numbers and find the minimum value in the tuple
+numbers = (1, 2, 3, 4, 5)
+min_value = min(numbers)
+#display the tuple and the minimum value
+print(f"Tuple: {numbers}")
+print(f"Minimum value: {min_value}")
+
+'''
+Output:
+Tuple: (1, 2, 3, 4, 5)
+Minimum value: 1
+'''
