fe702f5cf18a07b513229e5505796d20c427c661
kanishkamiet-commits <kanishkamiet@gmail.com>
2026-02-24 16:10:38 +0530

    this file contain tuple max value program

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style___24feb/tuple_max_value66.py
@@ -0,0 +1,13 @@
+#input a tuple of numbers and find the maximum value in the tuple
+numbers = (1, 2, 3, 4, 5)
+max_value = max(numbers)
+#display the tuple and the maximum value
+print(f"Tuple: {numbers}")
+#display the maximum value
+print(f"Maximum value: {max_value}")
+
+'''
+Output:
+Tuple: (1, 2, 3, 4, 5)
+Maximum value: 5
+'''
